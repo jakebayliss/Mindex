aa4ad710cef0a7af3b96277c1882f39e1aaff711
Jake Bayliss <bayliss.jw@gmail.com>
2023-04-24 10:41:29 +1000

    Move to Controllers. Remove src folder

---
+++ api/Infrastructure/ConfigureServices.cs
@@ -0,0 +1,29 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Interceptors;
+using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+	public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+		
+		services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+				builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+		services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+		services.AddScoped<ApplicationDbContextInitialiser>();
+
+		services.AddTransient<IDateTime, DateTimeService>();
+
+		return services;
+	}
+}
