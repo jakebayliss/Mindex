4fc23b8903adf6cea2cf715404671ab5714d85c0
Jake Bayliss <bayliss.jw@gmail.com>
2023-04-24 11:33:30 +1000

    Connected NSwag for api client generation

---
+++ api/Mindex/Controllers/HabitsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mindex.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HabitsController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult Hey()
+		{
+			return Ok("Hey");
+		}
+	}
+}
