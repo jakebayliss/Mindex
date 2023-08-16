32db3dd11386dd6830d60bc2adc3d4393f497fdf
Jake Bayliss <bayliss.jw@gmail.com>
2023-08-16 22:14:31 +1000

    Remove Maui app from solution

--- api/MauiApp1/Services/ServiceHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MauiApp1.Services;
-public static class ServiceHelper
-{
-	public static IServiceProvider Services { get; private set; }
-
-	public static void Initialize(IServiceProvider serviceProvider) => Services = serviceProvider;
-
-	public static T GetService<T>() => Services.GetService<T>();
-}
